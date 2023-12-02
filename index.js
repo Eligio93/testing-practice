2dcf97fc76d83d272aa23612fbcf0788b9361d33
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-02 11:24:57 +1100

    add reverse String Function

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ function capitalize(string){
   let lowerCase= string.toLowerCase();
   return lowerCase.charAt(0).toUpperCase()+lowerCase.slice(1);
 }
-module.exports=capitalize;
+function reverseString(string){ 
+  let newString="";
+  for(let i=string.length-1;i>=0;i--){    
+    newString+=string.charAt(i);
+  }
+  return newString
+}
+
+module.exports={capitalize,reverseString};
