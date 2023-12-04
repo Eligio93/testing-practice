f2b488bb9095e76227d93fb01d2fbe4190b27cb2
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-04 15:01:59 +1100

    add calculator object with operations

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ function reverseString(string){
   }
   return newString
 }
-
-module.exports={capitalize,reverseString};
+let calculator={
+  add:function(n1,n2){
+    return n1+n2
+  },
+  subtract:function(n1,n2){
+    return n1-n2;
+  },
+  divide:function(n1,n2){
+    return n1/n2;
+  },
+  multiply:function(n1,n2){
+    return n1*n2;
+  }
+}
+module.exports={capitalize,reverseString,calculator};
